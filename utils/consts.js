14e07b0a3746eb78e46a144de7a5b40dc7e34c65
Nikolay Fomenko <fomen_94@bk.ru>
2022-12-07 18:34:55 +0300

    hotfix: error

--- utils/consts.js
+++ utils/consts.js
@@ -1,13 +1,13 @@
-const badReqErr = 400;
-const authErr = 401;
+const badReqError = 400;
+const authError = 401;
 const forbErr = 403;
 const notFoundErr = 404;
 const conflictErr = 409;
 const serverErr = 500;
 
 module.exports = {
-  badReqErr,
-  authErr,
+  badReqError,
+  authError,
   forbErr,
   notFoundErr,
   conflictErr,
