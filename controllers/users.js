4ff5f5a71cc4ffcdcb0e267ad12aeb78cb1bc8a0
Nikolay Fomenko <fomen_94@bk.ru>
2022-12-07 22:12:15 +0300

    hotfix: исправление ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,7 @@ const login = async (req, res, next) => {
     res.cookie('jwt', token, {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
-      SameSite: 'None',
+      sameSite: 'None',
       secure: true,
     });
     return res.send({ email });
